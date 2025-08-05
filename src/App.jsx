2ba7ff399d1da20ee5821914110d93d69dd685ec
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-08-05 19:59:05 -0300

    feat
    coloquei uma nova fonte, baixei o bootstrap e bootstrap icons. criei um titulo, coloquei uma imagem e um botao

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className='display-3'>Calculadora de Dólar</h1>
+      <h1 className='display-3 notable-regular'>Calculadora de Dólar</h1>
       <img src='/public/dollar.png' width='50' />
+      <br />
+      <button className='btn btn-primary'>Click Aqui</button>
     </>
   )
 }
