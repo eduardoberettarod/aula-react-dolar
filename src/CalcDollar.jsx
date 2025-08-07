6d27c7185a4e77a70cb6281641b0d76f4a7be0b9
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-08-07 20:07:26 -0300

    feat
    adicionando função aos botões

--- src/CalcDollar.jsx
+++ src/CalcDollar.jsx
@@ -1,6 +1,13 @@
 import './CalcDollar.css'
 
-function CalcDollar(){
+function CalcDollar() {
+
+    const calcular = (function () {
+        const reais = document.getElementById('reais').value
+        const cotacao = document.getElementById('cotacao').value
+        total = reais / cotacao
+        alert(cotacao)
+    })
 
     return (
         <>
@@ -13,7 +20,7 @@ function CalcDollar(){
                     <label htmlFor='cotacao'>Cotação do Dólar</label>
                     <input type='text' className='form-control' id='cotacao' />
                 </div>
-                <button type='button' className='btn btn-primary w-100'>Calcular</button>
+                <button type='button' onClick={calcular} className='btn btn-primary w-100'>Calcular</button>
             </form>
         </>
     )
